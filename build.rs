e4a9e6747d27280f3b63dd3161924bc593145f20
Franziskus Kiefer <franziskuskiefer@gmail.com>
2017-09-29 10:16:35 +0200

    fix builds (maybe)

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::path::Path;
 
 fn main() {
-    let lib_dir = env::var("NSS_LIB_DIR").expect("Please set NSS_LIB_DIR");
-    println!("cargo:rustc-link-search=native={}", lib_dir);
+    if !Path::new("/usr/lib/libnss3.so").exists() {
+        let lib_dir = env::var("NSS_LIB_DIR").expect("Please set NSS_LIB_DIR");
+        println!("cargo:rustc-link-search=native={}", lib_dir);
+    }
 }
